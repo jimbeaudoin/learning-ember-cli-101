89c39d9c9171c6f698cb82ee75d5f1a065697d1e
Jimmy Beaudoin <contact@jim-beaudoin.com>
2015-04-06 14:12:18 -0400

    Add: Delete Friends

--- borrowers/app/routes/friends/show.js
+++ borrowers/app/routes/friends/show.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    delete: function(model) {
+      var _this = this;
+
+      model.destroyRecord().then(function() {
+        _this.transitionTo('friends.index');
+      });
+    }
+  }
 });
