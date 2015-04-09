0d348f4f9deea84b20a3047879719321d4ad3b0f
Jimmy Beaudoin <contact@jim-beaudoin.com>
2015-04-09 15:05:05 -0400

    Add: Fetching Friends Articles

--- borrowers/app/router.js
+++ borrowers/app/router.js
@@ -9,13 +9,10 @@ export default Router.map(function() {
   this.resource('friends', function() {
     this.route('new');
 
-    this.route('show', {
-      path: ':friend_id'
+    this.route('show', { path: ':friend_id' }, function() {
+      this.resource('articles', function() { });
     });
 
-    this.route('edit', {
-      path: ':friend_id/edit'
-    });
+    this.route('edit', { path: ':friend_id/edit' });
   });
-  this.resource('articles', function() {});
 });
